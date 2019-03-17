22f831f38891021ad472ada27f198a71dc0f718e
Johannes Goebel <GoebelJohannes@gmx.net>
2019-03-17 17:02:25 +0100

    added script tag

--- src/main/java/com/github/appreciated/ProgressCircle.java
+++ src/main/java/com/github/appreciated/ProgressCircle.java
@@ -3,18 +3,17 @@ package com.github.appreciated;
 import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.templatemodel.TemplateModel;
 
 @Tag("progressbar-circle")
-@JavaScript("frontend://../webjars/progressbar.js/1.0.1/dist/progressbar.js")
 @HtmlImport("frontend://com/github/appreciated/progressbar/progressbar-circle.html")
 public class ProgressCircle extends PolymerTemplate<ProgressCircle.ProgressCircleModel> implements HasSize {
 
     public ProgressCircle() {
         setWidth("300px");
         setHeight("300px");
+        getElement().getStyle().set("position", "relative");
     }
 
     public double getAnimate() {
